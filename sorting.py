e1c8a34cc1c13555a48035bce052927982205a15
huizhongxue <hxue1@bu.edu>
2023-05-26 13:38:57 -0400

    sorting.py

--- sorting.py
+++ sorting.py
@@ -2,7 +2,8 @@
 
 a = [5,3,4,2,1]
 
-def bubleSort(a):
+# bubble sort
+def bubbleSort(a):
     for i in range(0,len(a)-1,1):
         for j in range(i,-1,-1):
             if a[j] > a[j+1]:
@@ -11,4 +12,20 @@ def bubleSort(a):
                 a[j+1] = temp
     return a
 
-print(bubleSort(a))
+print(bubbleSort(a))
+
+# insertion sort
+def insertionSort(a):
+    for i in range(1, len(a), 1):
+        key = a[i]
+        j = i-1
+        while j >= 0 and a[j] > key:
+            temp = a[j]
+            a[j] = a[j+1]
+            a[j+1] = temp
+            j = j-1
+    return a
+
+print(insertionSort(a))
+
+
